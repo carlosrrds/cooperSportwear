c44f10c475ea336b4cb7e7af7889b7290c6ef88e
carloszazuu <carlos.rrds@zazuu.pet>
2024-08-22 07:00:27 -0300

    chore: configure environment and Next.js settings
    - Adicionado o arquivo `config.ts` para armazenar a variável de ambiente `NEXT_PUBLIC_API_URL`, permitindo o acesso à URL da API pública.
    - Configurado o arquivo `next.config.mjs` para permitir o carregamento de imagens remotas de vários domínios, incluindo `artwalk.vteximg.com.br`, `static.lojanba.com`, `static.netshoes.com.br`, e `imgnike-a.akamaihd.net`.
    - Definidas as configurações de imagem para garantir que o Next.js possa lidar corretamente com imagens carregadas de fontes externas.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'artwalk.vteximg.com.br',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'static.lojanba.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'static.netshoes.com.br',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'imgnike-a.akamaihd.net',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
